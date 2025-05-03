8b5bd90b57de24214d551e5d38d3541e0fa86000
MohVij <mohanmoni16n@gmail.com>
2025-05-03 16:00:14 +0530

    initial commit

---
+++ mohvij-love-app/src/main/java/com/example/mohvij/MohvijApplication.java
@@ -0,0 +1,20 @@
+package com.example.mohvij;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+@Controller
+public class MohvijApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MohvijApplication.class, args);
+    }
+
+    @GetMapping("/")
+    public String home() {
+        return "index";
+    }
+}
